656483ed2411cafd6d55b32d3f4cb6f5662b8184
Xu Ma <xma3@ebay.com>
2022-07-20 15:44:15 -0700

    Update demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -110,6 +110,7 @@ if __name__ == "__main__":
             img = read_image(path, format="BGR")
             start_time = time.time()
             predictions, visualized_output = demo.run_on_image(img)
+            print(f"The prediction results are: \n{predictions}")
             logger.info(
                 "{}: {} in {:.2f}s".format(
                     path,
